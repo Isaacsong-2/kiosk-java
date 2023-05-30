fd35858b6929633f6522f002f70a764797245dce
SongIsak <thddltkr10@naver.com>
2023-05-30 21:36:25 +0900

    리팩터링

--- src/ui/Home.java
+++ src/ui/Home.java
@@ -28,7 +28,7 @@ public class Home {
 
         while(true){
             displayMenu(); // 초기 메뉴화면
-            inputNumber = selectMenu(0, 6);
+            inputNumber = selectMenu(6);
             switch(inputNumber){
                 case 0:
                     salesRecord(); // 판매기록 조회
@@ -46,22 +46,24 @@ public class Home {
         }
     }
 
-    private int selectMenu(int minNum, int maxNum) {
+    private int selectMenu(int maxNum) { // minNum과 maxNum사이의 정수입력을 받을때까지 입력을 받음
         while(true) {
             System.out.print("Select : ");
             String line = scanner.nextLine();
-            Integer menuNumber = Integer.MAX_VALUE;
-            try {
-                menuNumber = Integer.parseInt(line);
-            } catch (NumberFormatException e) {
-                System.out.println("입력을 확인하세요");
-                continue;
-            }
-            if (menuNumber > maxNum || menuNumber < minNum) {
+            Integer menuNumber = isValid(line);
+            if (menuNumber > maxNum || menuNumber < 0) {
                 System.out.println("숫자를 확인해주세요");
                 continue;
             }
             return menuNumber;
+        }
+    }
+
+    private int isValid(String line){
+        try{
+            return Integer.parseInt(line);
+        } catch(NumberFormatException e){
+            return Integer.MAX_VALUE;
         }
     }
 
@@ -96,26 +98,32 @@ public class Home {
         System.out.println("[ " + categoryName + " MENU ]");
         for (int i = 0; i < products.size(); i++) {
             Product product = products.get(i);
-            String line = String.format("%d. %-15s | W %5d | %s", (i + 1), product.getName(), product.getPrice(), product.getDetail());
+            String line = String.format("%d. %-15s | W %5d | %s", i+1, product.getName(), product.getPrice(), product.getDetail());
             System.out.println(line);
         }
         System.out.println();
+        System.out.println("0. 뒤로가기");
         System.out.println("------------------------------------------------");
 
-        int inputNumber = selectMenu(1, products.size());
+        int inputNumber = selectMenu(products.size());
+        if (inputNumber == 0) return;
+        displayAdd(inputNumber, products);
+    }
+
+    private void displayAdd(int inputNumber, ArrayList<Product> products){
         Product selectedProduct = products.get(inputNumber-1); // product list중 선택한 product
         String line = String.format("\"%-15s | W %5d | %s\"", selectedProduct.getName(), selectedProduct.getPrice(), selectedProduct.getDetail());
         System.out.println(line);
         System.out.println("위 메뉴를 장바구니에 추가하시겠습니까?");
-        System.out.println("1. 확인        2. 취소");
+        System.out.println("1. 확인        0. 취소");
         System.out.println("------------------------------------------------");
 
-        inputNumber = selectMenu(1, 2);
+        inputNumber = selectMenu(1);
         if (inputNumber == 1){
             order.addProduct(selectedProduct); // 선택한 product를 장바구니(order)에 추가
             System.out.println(selectedProduct.getName()+" 가 장바구니에 추가되었습니다.");
         }
-    }
+    };
 
     private void displayOrder(){
         System.out.println("------------------------------------------------");
@@ -132,13 +140,14 @@ public class Home {
         System.out.println();
         System.out.println("[ Total ]");
         System.out.println("W " + order.getTotalPrice() + "원");
-//        order.showOrder();
         System.out.println();
 
-        System.out.println("1. 주문      2. 메뉴판");
+        System.out.println("1. 주문      0. 메뉴판");
         System.out.println("------------------------------------------------");
 
-        int inputNumber = selectMenu(1, 2);
+        int inputNumber = selectMenu(1);
+        if (inputNumber == 0) return;
+
         if (inputNumber == 1) {
             soldAmount.increaseTotalSale(order.getTotalPrice()); // 총 판매금액에 현재 장바구니에 담겨진 상품들의 총 금액을 더해줌
             soldList.addSoldList(order.getProducts()); // 판매목록에 현재 장바구니에 담겨진 상품들을 추가
@@ -152,9 +161,7 @@ public class Home {
             System.out.println("3초후 메뉴판으로 돌아갑니다.");
             try {
                 Thread.sleep(3000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            } catch (InterruptedException e){}
         }
     }
 
@@ -162,10 +169,10 @@ public class Home {
         System.out.println("------------------------------------------------");
 
         System.out.println("진행하던 주문을 취소하시겠습니까?");
-        System.out.println("1. 확인        2. 취소");
+        System.out.println("1. 확인        0. 취소");
         System.out.println("------------------------------------------------");
 
-        int inputNumber = selectMenu(1, 2);
+        int inputNumber = selectMenu(1);
         if (inputNumber == 1){
             System.out.println("진행하던 주문이 취소되었습니다.");
             order.removeOrder(); // 주문취소(장바구니 비우기)
@@ -185,6 +192,7 @@ public class Home {
             System.out.println(String.format("- %-15s | W %5d | %d", product.getName(), product.getPrice(), quantity));
         }));
         System.out.println("------------------------------------------------");
-        System.out.println("1. 돌아가기");
+        System.out.println("0. 돌아가기");
+        selectMenu(0);
     }
 }
